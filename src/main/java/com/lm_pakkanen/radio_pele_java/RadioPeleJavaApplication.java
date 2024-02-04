085c66cdfdb7cc1634bc71483d31958a0a28d42c
lm-pakkanen <kuikka87@gmail.com>
2024-02-04 16:09:24 +0200

    Initialise spring boot

---
+++ src/main/java/com/lm_pakkanen/radio_pele_java/RadioPeleJavaApplication.java
@@ -0,0 +1,13 @@
+package com.lm_pakkanen.radio_pele_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioPeleJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadioPeleJavaApplication.class, args);
+	}
+
+}
