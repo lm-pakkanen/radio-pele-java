764680101adb7eea25cae0590931b0657418cdab
lm-pakkanen <kuikka87@gmail.com>
2024-02-05 19:14:16 +0200

    Implement invalid channel exception

---
+++ src/main/java/com/lm_pakkanen/radio_pele_java/models/exceptions/InvalidChannelException.java
@@ -0,0 +1,7 @@
+package com.lm_pakkanen.radio_pele_java.models.exceptions;
+
+public class InvalidChannelException extends Exception {
+  public InvalidChannelException() {
+    super("Invalid channel.");
+  }
+}
