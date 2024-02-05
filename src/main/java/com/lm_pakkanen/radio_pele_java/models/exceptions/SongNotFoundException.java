fe10b21fcee5ec1c45f85c3ae9e5eb29c7ae034a
lm-pakkanen <kuikka87@gmail.com>
2024-02-05 19:13:33 +0200

    Replace exception with new one

--- src/main/java/com/lm_pakkanen/radio_pele_java/models/exceptions/SongNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.lm_pakkanen.radio_pele_java.models.exceptions;
-
-public final class SongNotFoundException extends Exception {
-  public SongNotFoundException() {
-    super("Song not found!");
-  }
-}
