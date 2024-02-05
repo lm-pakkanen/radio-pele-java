e010ae93c3ef7370b70022e652ed44b24d852756
lm-pakkanen <kuikka87@gmail.com>
2024-02-05 19:14:06 +0200

    Add methods to ICommandListener

--- src/main/java/com/lm_pakkanen/radio_pele_java/interfaces/ICommandListener.java
+++ src/main/java/com/lm_pakkanen/radio_pele_java/interfaces/ICommandListener.java
@@ -1,5 +1,12 @@
 package com.lm_pakkanen.radio_pele_java.interfaces;
 
 import net.dv8tion.jda.api.hooks.EventListener;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 
-public interface ICommandListener extends EventListener {}
+public interface ICommandListener extends EventListener {
+  public String getCommandName();
+
+  public String getCommandDescription();
+
+  public SlashCommandData getCommandData();
+}
