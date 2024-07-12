090f127a418b3b425b1a9d8fac44747f59228ad1
Lauri-Mikael Pakkanen <lm.r.pakkanen@gmail.com>
2024-07-12 14:33:18 +0000

    Run killall with sudo

--- run-prod.sh
+++ run-prod.sh
@@ -1,1 +1,1 @@
-killall -9 java || true && mvn clean package -Dmaven.test.skip=true && java -jar -noverify target/radio_pele_java-0.0.1-SNAPSHOT.jar &
+sudo killall -9 java || true && mvn clean package -Dmaven.test.skip=true && java -jar -noverify target/radio_pele_java-0.0.1-SNAPSHOT.jar &
