2ad4cbc23f1940066223a21bfecfab7018a16502
Lauri-Mikael Pakkanen <lm.r.pakkanen@gmail.com>
2025-06-07 15:15:58 +0000

    Update scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,2 @@
 set -e
-sudo killall -9 java || true &&
-  ./mvnw clean package -Dmaven.test.skip=true &&
-  java -jar -noverify target/radio_pele_java-0.0.1-SNAPSHOT.jar
+java -jar -noverify target/radio_pele_java-0.0.1-SNAPSHOT.jar
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
+docker ps -aq | xargs docker stop || true
 docker build . -t "radio-pele-java"
 docker run -it radio-pele-java --name radio-pele-java --rm
