9ddb5b26250292c1c8ac124af9ecba0eabff44fa
Lauri-Mikael Pakkanen <lm.r.pakkanen@gmail.com>
2025-07-01 08:34:07 +0000

    Refactor to Kotlin

--- bot/src/test/java/com/lm_pakkanen/radio_pele_java/models/TrackInfoTest.kt
+++ bot/src/test/java/com/lm_pakkanen/radio_pele_java/models/TrackInfoTest.kt
@@ -1,47 +1,45 @@
-package com.lm_pakkanen.radio_pele_java.models;
+package com.lm_pakkanen.radio_pele_java.models
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.AbstractMap;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Configuration
+import org.springframework.lang.NonNull
+import java.util.*
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-class TrackInfoTest {
+internal class TrackInfoTest {
 
   @Test
   @DisplayName("Test TrackInfo.formatDuration")
-  void testFormatDuration() {
+  fun testFormatDuration() {
+    val twoMinutesInMs = (2 * 60 * 1000).toLong()
 
-    final long twoMinutesInMs = 2 * 60 * 1000;
-
-    final List<AbstractMap.SimpleEntry<Long, String>> durationsInMs = new ArrayList<>() {
-      {
-        add(createEntry(-1 * twoMinutesInMs, "<n/a>"));
-        add(createEntry(0, "<n/a>"));
-        add(createEntry(twoMinutesInMs, "2min"));
-        add(createEntry(twoMinutesInMs + 3 * 1000, "2min3s"));
-        add(createEntry(twoMinutesInMs + 30 * 1000, "2min30s"));
+    val durationsInMs: MutableList<AbstractMap.SimpleEntry<Long, String>> =
+      object : ArrayList<AbstractMap.SimpleEntry<Long, String>>() {
+        init {
+          add(createEntry(-1 * twoMinutesInMs, "<n/a>"))
+          add(createEntry(0, "<n/a>"))
+          add(createEntry(twoMinutesInMs, "2min"))
+          add(createEntry(twoMinutesInMs + 3 * 1000, "2min3s"))
+          add(createEntry(twoMinutesInMs + 30 * 1000, "2min30s"))
+        }
       }
-    };
 
-    for (final AbstractMap.SimpleEntry<Long, String> durationInMs : durationsInMs) {
-      String formatted = RefinedTrackInfo.formatDuration(durationInMs.getKey());
-      assertEquals(durationInMs.getValue(), formatted);
+    for (durationInMs in durationsInMs) {
+      val formatted = RefinedTrackInfo.formatDuration(durationInMs.key!!)
+      Assertions.assertEquals(durationInMs.value, formatted)
     }
   }
 
-  private @NonNull AbstractMap.SimpleEntry<Long, String> createEntry(
-      long durationMs, @NonNull String expectedDurationString) {
-    return new AbstractMap.SimpleEntry<>(durationMs, expectedDurationString);
+  @NonNull
+  private fun createEntry(
+    durationMs: Long, @NonNull expectedDurationString: String
+  ): AbstractMap.SimpleEntry<Long, String> {
+    return AbstractMap.SimpleEntry<Long, String>(durationMs, expectedDurationString)
   }
 
   @Configuration
-  public static class Config {}
+  open class Config
 }
