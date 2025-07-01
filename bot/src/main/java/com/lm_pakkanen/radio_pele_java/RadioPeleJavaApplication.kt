aebb25f83e8a92c9f7bd80df9ea69522a95f1b29
Lauri-Mikael Pakkanen <lm.r.pakkanen@gmail.com>
2025-07-01 08:34:00 +0000

    Refactor to Kotlin

--- bot/src/main/java/com/lm_pakkanen/radio_pele_java/RadioPeleJavaApplication.kt
+++ bot/src/main/java/com/lm_pakkanen/radio_pele_java/RadioPeleJavaApplication.kt
@@ -1,12 +1,12 @@
-package com.lm_pakkanen.radio_pele_java;
+package com.lm_pakkanen.radio_pele_java
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-public class RadioPeleJavaApplication {
+open class RadioPeleJavaApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(RadioPeleJavaApplication.class, args);
+  fun main(args: Array<String>) {
+    SpringApplication.run(RadioPeleJavaApplication::class.java, *args)
   }
 }
