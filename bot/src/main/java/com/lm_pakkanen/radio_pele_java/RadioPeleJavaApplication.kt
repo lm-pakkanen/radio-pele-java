fdb6103fcb911dc51801cf5005c24a8256227db6
Lauri-Mikael Pakkanen <lm.r.pakkanen@gmail.com>
2025-07-01 09:41:43 +0000

    Revert "Refactor to Kotlin"
    This reverts commit aebb25f83e8a92c9f7bd80df9ea69522a95f1b29.

--- bot/src/main/java/com/lm_pakkanen/radio_pele_java/RadioPeleJavaApplication.kt
+++ bot/src/main/java/com/lm_pakkanen/radio_pele_java/RadioPeleJavaApplication.kt
@@ -1,12 +1,12 @@
-package com.lm_pakkanen.radio_pele_java
+package com.lm_pakkanen.radio_pele_java;
 
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-open class RadioPeleJavaApplication {
+public class RadioPeleJavaApplication {
 
-  fun main(args: Array<String>) {
-    SpringApplication.run(RadioPeleJavaApplication::class.java, *args)
+  public static void main(String[] args) {
+    SpringApplication.run(RadioPeleJavaApplication.class, args);
   }
 }
