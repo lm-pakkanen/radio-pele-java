7b9498d6071caca5d47611624b07c109d77585a3
Lauri-Mikael Pakkanen <lm.r.pakkanen@gmail.com>
2025-07-26 08:43:43 +0000

    Update artifactory script

--- run-artifactory.sh
+++ run-artifactory.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-docker pull us-east1-docker.pkg.dev/flash-bazaar-487/radio-pele/latest
-docker container run \
-    --rm \
-    --detach \
-    us-east1-docker.pkg.dev/flash-bazaar-487/radio-pele/latest
+docker compose pull #us-east1-docker.pkg.dev/flash-bazaar-487/radio-pele/latest
+docker compose up --no-build -d
 
