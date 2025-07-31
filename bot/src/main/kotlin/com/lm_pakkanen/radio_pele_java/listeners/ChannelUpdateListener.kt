111d7a4864c683c9e453c7156bb19dd888a31277
Lauri-Mikael Pakkanen <lm.r.pakkanen@gmail.com>
2025-07-31 16:53:18 +0000

    Fix leaving the channel after other users have disconnected

--- bot/src/main/kotlin/com/lm_pakkanen/radio_pele_java/listeners/ChannelUpdateListener.kt
+++ bot/src/main/kotlin/com/lm_pakkanen/radio_pele_java/listeners/ChannelUpdateListener.kt
@@ -1,8 +1,9 @@
-package com.lm_pakkanen.radio_pele_java.controllers.listeners
+package com.lm_pakkanen.radio_pele_java.listeners
 
 import com.lm_pakkanen.radio_pele_java.controllers.TrackScheduler
 import com.lm_pakkanen.radio_pele_java.interfaces.IEventListener
 import io.github.oshai.kotlinlogging.KotlinLogging
+import net.dv8tion.jda.api.entities.GuildVoiceState
 import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel
 import net.dv8tion.jda.api.events.GenericEvent
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent
@@ -30,25 +31,33 @@ class ChannelUpdateListener(
     }
 
     val chanUserLeftFrom: AudioChannel? = event.channelJoined
-    val audioManager = event.getGuild().audioManager
-    val connectedAudioChan: AudioChannel? = audioManager.connectedChannel
 
-    log.debug { "User left from channel '$chanUserLeftFrom'" }
-    log.debug { "Bot's current channel: '$connectedAudioChan'" }
-
-    if (chanUserLeftFrom == null || connectedAudioChan == null) {
+    if (chanUserLeftFrom == null) {
       // Don't care
-      log.debug { "No user channel or bot channel available" }
+      log.debug { "No user channel available" }
       return
     }
 
-    if (chanUserLeftFrom.id != connectedAudioChan.id) {
+    val botVoiceState: GuildVoiceState? = event.guild.selfMember.voiceState
+
+    if (botVoiceState == null) {
+      // Don't care
+      log.debug { "No bot channel available" }
+      return
+    }
+
+    val botChan: AudioChannel = botVoiceState.channel!!
+
+    log.debug { "User left from channel '$chanUserLeftFrom.name'" }
+    log.debug { "Bot's current channel: '$botChan.name'" }
+
+    if (chanUserLeftFrom.id != botChan.id) {
       // Don't care
       log.debug { "User left from channel other than bot channel" }
       return
     }
 
-    val membersInChan = connectedAudioChan.members
+    val membersInChan = botChan.members
     val isAllBotsInChan = membersInChan.stream().allMatch { member -> member.user.isBot }
 
     log.debug { "Only bots left in the channel: $isAllBotsInChan" }
@@ -56,8 +65,7 @@ class ChannelUpdateListener(
     if (isAllBotsInChan) {
       // Clear queue and leave channel if only bots left in the channel
       trackScheduler.destroy()
-      audioManager.closeAudioConnection()
-      audioManager.sendingHandler = null
+      event.jda.directAudioController.disconnect(event.guild)
     }
   }
 }
