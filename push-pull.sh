4881bac4e01e5fdd148fa8b0a8a39865cd47c979
Lauri-Mikael Pakkanen <lm.r.pakkanen@gmail.com>
2025-09-14 12:51:35 +0000

    Add profile to build script

--- push-pull.sh
+++ push-pull.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-docker compose build
+docker compose --profile "prod" build
 docker tag radio-pele-java-bot us-east1-docker.pkg.dev/flash-bazaar-487/radio-pele/radio-pele-bot:latest
 docker push us-east1-docker.pkg.dev/flash-bazaar-487/radio-pele/radio-pele-bot:latest
 
